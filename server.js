f660bc364e098ae6d47efe280391726986bf2a09
Yaroslaw <ersddfxdfghh@gmail.com>
2025-05-21 10:57:25 +0300

    fredis

--- server.js
+++ server.js
@@ -3,34 +3,8 @@ const cors= require( 'cors');
 const bodyParser= require( 'body-parser');
 const sqlite3= require( 'sqlite3');
 const bcrypt= require( 'bcryptjs');
-const session= require( 'express-session');
 const httpsLocalhost=  require( "https-localhost");
-const https =require('https');
-const fs =require( 'fs');
-const redis = require('redis');
-const { RedisStore } = require('connect-redis');
 const app = express();
-console.log(process.env.REDIS_URL);
-const redisClient = redis.createClient({
-    url: process.env.REDIS_URL,
-});
-redisClient.connect().catch(console.error);
-let redisStore = new RedisStore({
-  client: redisClient,
-});
-// redisClient.on('error', (err) => {
-//   console.error('Redis error:', err);
-// });
-app.use(session({
-  store: redisStore,
-  secret: process.env.SESSION_SECRET || '@45erere/;:67WER&ER9(304_DEff#Efdgdf',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    secure: process.env.NODE_ENV === 'production',
-    maxAge: 24 * 60 * 60 * 1000
-  }
-}));
 const { verbose } = sqlite3;
 const httpsServer = httpsLocalhost();
 const corsOptions = {
@@ -52,18 +26,17 @@ app.post('/api/check', (req, res) => {
     else{
           let passwordMatch = await bcrypt.compare(req.body.password,result[0].password);
           if(passwordMatch){
-            req.session.userId = result[0].id;
-            res.end(JSON.stringify(true));
+            res.end(JSON.stringify(result[0].id));
           }else{
             res.end(JSON.stringify(false));
           }
     }
   });
 });
-app.post('/api/addork', (req, res) => {
-    if(req.session.userId){
+app.post('/api/addWork', (req, res) => {
+    if(req.body.userId){
       let date=new Date();
-      const sql = `INSERT INTO \`profile\` (\`name\`, \`tag\`,\`kategory\`,\`date\`,\`userId\`,\`type\`,\`like\`) VALUES ('${req.body.name}', '${req.body.tag}','${req.body.kategory}','${date}','${req.session.userId}','${req.body.type}',0)`;
+      const sql = `INSERT INTO \`profile\` (\`name\`, \`tag\`,\`kategory\`,\`date\`,\`userId\`,\`type\`,\`like\`) VALUES ('${req.body.name}', '${req.body.tag}','${req.body.kategory}','${date}','${req.body.userId}','${req.body.type}',0)`;
       db.run(sql, async function(err, result) {
         res.end(JSON.stringify(true));
       });
@@ -73,26 +46,26 @@ app.post('/api/addork', (req, res) => {
     }
 });
 app.post('/api/addVack', (req, res) => {
-    if(req.session.userId){
+    if(req.body.userId){
       let date=new Date();
-      const sql = `INSERT INTO \`Vacansi\` (\`name\`, \`about\`,\`cost\`,\`need\`,\`userId\`,\`type\`,\`location\`,\`date\`) VALUES ('${req.body.name}', '${req.body.about}','${req.body.cost}','${req.body.need}','${req.session.userId}','${req.body.type}','${req.body.location}','${date}')`;
+      const sql = `INSERT INTO \`Vacansi\` (\`name\`, \`about\`,\`cost\`,\`need\`,\`userId\`,\`type\`,\`location\`,\`date\`) VALUES ('${req.body.name}', '${req.body.about}','${req.body.cost}','${req.body.need}','${req.body.userId}','${req.body.type}','${req.body.location}','${date}')`;
       db.run(sql, async function(err, result) {
         res.end(JSON.stringify(true));
       });
-    }
+    }  
     else{
       res.end(JSON.stringify(false));
     }
 });
 app.post('/api/addComm', (req, res) => {
-    if(req.session.userId && req.session.workId){
-      const sqlCheck = `SELECT * FROM \`Option\` WHERE \`profileID\`='${req.session.workId}' AND \`userId\`=${req.session.userId}`;
+    if(req.body.userId && req.body.workId){
+      const sqlCheck = `SELECT * FROM \`Option\` WHERE \`profileID\`='${req.body.workId}' AND \`userId\`=${req.body.userId}`;
       db.all(sqlCheck, async function(err, resultSelect) {
         console.log(!resultSelect[0]);
                 console.log(!resultSelect[0]);
         if(!resultSelect[0]){
           let date=new Date();
-          const sql = `INSERT INTO \`Option\` (\`text\`,\`profileID\`,\`userId\`,\`date\`) VALUES ('${req.body.text}', '${req.session.workId}','${req.session.userId}','${date}')`;
+          const sql = `INSERT INTO \`Option\` (\`text\`,\`profileID\`,\`userId\`,\`date\`) VALUES ('${req.body.text}', '${req.body.workId}','${req.body.userId}','${date}')`;
           db.run(sql, async function(err, result) {
             res.end(JSON.stringify(true));
           });
@@ -107,26 +80,6 @@ app.post('/api/addComm', (req, res) => {
       res.end(JSON.stringify(false));
     }
 });
-app.post('/api/setId', (req, res) => {
-    if(req.session.userId){
-      req.session.vackId = null;
-      req.session.workId = req.body.workId;
-      res.end(JSON.stringify(true));
-    }
-    else{
-      res.end(JSON.stringify(false));
-    }
-});
-app.post('/api/setVackId', (req, res) => {
-    if(req.session.userId){
-      req.session.workId = null;
-      req.session.vackId = req.body.workId;
-      res.end(JSON.stringify(true));
-    }
-    else{
-      res.end(JSON.stringify(false));
-    }
-});
 app.post('/api/addUser', (req, res) => {
   const sql = `SELECT * FROM \`user\` WHERE \`name\`='${req.body.name}'`;
   db.all(sql, async function(err, result) {
@@ -136,8 +89,7 @@ app.post('/api/addUser', (req, res) => {
         const sqlAdd = `INSERT INTO \`user\` (\`name\`, \`password\`,\`role\`,\`email\`,\`phone\`,\`date\`) VALUES ('${req.body.name}', '${hashedPassword}','${req.body.role}','${req.body.email}','${req.body.phone}','${date}')`;
           db.run(sqlAdd, async function(err, resultAdd) {
              db.all(sql, async function(err, resultSelect) {
-                req.session.userId = resultSelect[0].id;
-                res.end(JSON.stringify(true));
+                res.end(JSON.stringify(resultSelect[0].id));
             })
           })
     }
@@ -146,25 +98,9 @@ app.post('/api/addUser', (req, res) => {
     }
   });
 });
-app.get('/api/isAutificating', (req, res) => {
-    if(req.session.userId){
-      res.end(JSON.stringify(true));
-    }
-    else{
-      res.end(JSON.stringify(false));
-    }
-});
-app.get('/api/Exit', (req, res) => {
-  req.session.destroy((err) => {
-    if (err) {
-      console.error('Ошибка при уничтожении сессии:', err);
-    }
-  });
-  res.end();
-});
 app.post('/api/getProfil', (req, res) => {
-  if(req.session.userId){
-    const sql = `SELECT * FROM \`user\` WHERE \`id\`='${req.session.userId}'`;
+  if(req.body.userId){
+    const sql = `SELECT * FROM \`user\` WHERE \`id\`='${req.body.userId}'`;
     db.all(sql, async function(err, result) {
       res.end(JSON.stringify(result));
   });
@@ -174,8 +110,8 @@ app.post('/api/getProfil', (req, res) => {
   }
 });
 app.post('/api/getWork', (req, res) => {
-  if(req.session.userId){
-    const sql = `SELECT * FROM \`profile\` WHERE \`userId\`=${req.session.userId}`;
+  if(req.body.userId){
+    const sql = `SELECT * FROM \`profile\` WHERE \`userId\`=${req.body.userId}`;
     db.all(sql, async function(err, result) {
       res.end(JSON.stringify(result));
   });
@@ -185,8 +121,8 @@ app.post('/api/getWork', (req, res) => {
   }
 });
 app.post('/api/getVack', (req, res) => {
-  if(req.session.userId){
-    const sql = `SELECT * FROM \`Vacansi\` WHERE \`userId\`=${req.session.userId}`;
+  if(req.body.userId){
+    const sql = `SELECT * FROM \`Vacansi\` WHERE \`userId\`=${req.body.userId}`;
     db.all(sql, async function(err, result) {
       res.end(JSON.stringify(result));
   });
@@ -196,8 +132,8 @@ app.post('/api/getVack', (req, res) => {
   }
 });
 app.post('/api/getOneWork', (req, res) => {
-  if(req.session.workId){
-    const sql = `SELECT * FROM \`profile\` WHERE \`id_profile\`='${req.session.workId}'`;
+  if(req.body.workId){
+    const sql = `SELECT * FROM \`profile\` WHERE \`id_profile\`='${req.body.workId}'`;
     db.all(sql, async function(err, result) {
       res.end(JSON.stringify(result));
     });
@@ -207,8 +143,8 @@ app.post('/api/getOneWork', (req, res) => {
   }
 });
 app.post('/api/getOneVack', (req, res) => {
-  if(req.session.vackId){
-    const sql = `SELECT * FROM \`Vacansi\` WHERE \`id\`='${req.session.vackId}'`;
+  if(req.body.vackId){
+    const sql = `SELECT * FROM \`Vacansi\` WHERE \`id\`='${req.body.vackId}'`;
     db.all(sql, async function(err, result) {
       res.end(JSON.stringify(result));
     });
@@ -218,8 +154,8 @@ app.post('/api/getOneVack', (req, res) => {
   }
 });
 app.post('/api/getComm', (req, res) => {
-  if(req.session.workId){
-    const sql = `SELECT * FROM \`Option\` JOIN \`user\` ON \`user\`.\`id\`= \`Option\`.\`userId\` WHERE \`Option\`.\`profileID\`='${req.session.workId}'`;
+  if(req.body.workId){
+    const sql = `SELECT * FROM \`Option\` JOIN \`user\` ON \`user\`.\`id\`= \`Option\`.\`userId\` WHERE \`Option\`.\`profileID\`='${req.body.workId}'`;
     db.all(sql, async function(err, result) {
       res.end(JSON.stringify(result));
     });
@@ -229,21 +165,11 @@ app.post('/api/getComm', (req, res) => {
   }
 });
 app.post('/api/getOneLike', async (req, res) => {
-  if(req.session.workId){
-    let step;
-    const sql = `SELECT * FROM \`profile\` WHERE \`id_profile\`='${req.session.workId}'`;
-    const likeSessionKey = `like_${req.session.workId}`;
-    if(!req.session[likeSessionKey]){
-      req.session[likeSessionKey]=true;
-      step=1;
-    }
-    else{
-      req.session[likeSessionKey]=false;
-      step=-1;
-    }
-    await db.all(sql, async function(err, result) {
-      let like=result[0].like+step;
-      let sqlUpdate=`UPDATE \`profile\` SET \`like\` = ${like} WHERE \`id_profile\`='${req.session.workId}'`;
+  if(req.body.workId){
+    const sql = `SELECT * FROM \`profile\` WHERE \`id_profile\`='${req.body.workId}'`;
+    db.all(sql, async function(err, result) {
+      let like=result[0].like+req.body.step;
+      let sqlUpdate=`UPDATE \`profile\` SET \`like\` = ${like} WHERE \`id_profile\`='${req.body.workId}'`;
       db.run(sqlUpdate, async function(err, result) {
         res.end(JSON.stringify(like));
       });
@@ -254,41 +180,26 @@ app.post('/api/getOneLike', async (req, res) => {
   }
 });
 app.post('/api/getAllKat', (req, res) => {
-  if(req.session.userId){
     const sql = `SELECT \`kategory\` FROM \`profile\` GROUP BY \`kategory\``;
     db.all(sql, async function(err, result) {
       res.end(JSON.stringify(result));
     });
-  }
-  else{
-    res.end(JSON.stringify(false));
-  }
 });
 app.post('/api/getAllType', (req, res) => {
-  if(req.session.userId){
     const sql = `SELECT \`type\` FROM \`Vacansi\` GROUP BY \`type\``;
     db.all(sql, async function(err, result) {
       res.end(JSON.stringify(result));
     });
-  }
-  else{
-    res.end(JSON.stringify(false));
-  }
 });
 app.post('/api/getAllDate', (req, res) => {
-  if(req.session.userId){
     const sql = `SELECT \`date\` FROM \`Vacansi\` GROUP BY \`date\``;
     db.all(sql, async function(err, result) {
       res.end(JSON.stringify(result));
     });
-  }
-  else{
-    res.end(JSON.stringify(false));
-  }
 });
 app.post('/api/getAllWork', (req, res) => {
-  if(req.session.userId){
-    const sql = `SELECT * FROM \`user\` WHERE \`id\`='${req.session.userId}'`;
+  if(req.body.userId){
+    const sql = `SELECT * FROM \`user\` WHERE \`id\`='${req.body.userId}'`;
     db.all(sql, async function(err, result) {
       let sqlSelect='';
       if(result[0].role=="boss" && !req.body.select){
@@ -313,7 +224,6 @@ app.post('/api/getAllWork', (req, res) => {
   }
 });
 app.post('/api/getAllVack', (req, res) => {
-  if(req.session.userId){
     let sql = ``;
     if(!req.body.select && !req.body.selectDate){
       sql = `SELECT * FROM \`Vacansi\``;
@@ -330,10 +240,6 @@ app.post('/api/getAllVack', (req, res) => {
     db.all(sql, async function(err, result) {
       res.end(JSON.stringify(result));
     });
-  }
-  else{
-    res.end(JSON.stringify(false));
-  }
 });
 // const options = {
 //   key: fs.readFileSync('localhost-key.pem'),
